af588e408f1084ebd1d0fbca3b9da085f89531a7
Sittravell <sittravellavenger@gmail.com>
2024-04-10 23:51:46 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ use App\Http\Middleware\AuthenticateParser;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/posts', [PostController::class, 'index']);
-
+Route::get('/test', function (){
+    return 'lol';
+});
 Route::middleware(AuthenticateParser::class)->group(function () {
     Route::post('/posts', [PostController::class, 'store']);
 });
